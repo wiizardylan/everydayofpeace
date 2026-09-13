6246c989322d6cf22f40dcc5a3280e04aea1cf37
wiizardylan <dylancarter1047@gmail.com>
2026-09-13 01:57:29 -0400

    Initial commit

---
+++ public_sketch/sketch.js
@@ -0,0 +1,40 @@
+var socket;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  
+  socket = io.connect("http://localhost:3000");
+  socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+  r = random(255);
+  g = random(255);
+  b = random(255);
+  a = random(120, 15);
+
+  noStroke();
+  fill(r,g,b,a);
+  ellipse(data.x, data.y, 15, 15);
+}
+
+function mouseDragged() {
+  console.log("Sending: " + mouseX, + ',' + mouseY);
+
+  var data ={
+    x: mouseX,
+    y: mouseY,
+  }
+
+  socket.emit('mouse', data);
+
+  noStroke();
+  fill(211,211,211);
+  if (mouseIsPressed == true) {
+  ellipse(mouseX, mouseY, 15, 15);
+}
+}
+
+function draw() {
+}
